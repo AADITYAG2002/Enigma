1bf8788b56ed8297741514c3f5f20491d0250210
AADITYAG2002 <aadityag2002@gmail.com>
2023-04-10 21:59:11 +0530

    plug settings needed

--- crack.py
+++ crack.py
@@ -1,10 +1,46 @@
-from settings import *
 import enigma_bare
 
 # 25 4 1 || ka ti || aaditya => WNRLDXK
 
-enc_text = "aaditya"
-plugs = "ka ti"
-rotors = [25,4,1]
-txt = enigma_bare.Enigma.encrypt(rotors[0],rotors[1],rotors[2],plugs,enc_text)
-print(txt)
+enc_text = "WNRLDXK"
+pln_text = "AADITYA"
+plugs = ""
+rotors = [0,0,0]
+
+def check_plugs(plugs):
+    plugs = plugs.split()
+    for i in range(len(plugs)):
+        for j in range(i+1,len(plugs)):
+            if plugs[j].find(plugs[i][0]) != -1 or plugs[j].find(plugs[i][1]) != -1:
+                return False
+    return True
+
+def rotor_shift(rotors):
+    shift_2 = True if rotors[2] == 25 else False
+    shift_3 = True if rotors[2] == rotors[1] == 25 else False
+    rotors[2] += 1
+    rotors[2] %= 26
+    if shift_2:
+        rotors[1] += 1
+        rotors[1] %= 26
+    if shift_3:
+        rotors[0] += 1
+        rotors[0] %= 26
+
+
+i = 0
+while i < len(enc_text):
+    converted_letter = enigma_bare.Enigma.encrypt(rotors[0],rotors[1],rotors[2],plugs,enc_text[i])
+    if converted_letter != pln_text[i]:
+        plugs += converted_letter + pln_text[i] + ' '
+        if check_plugs(plugs):
+            print(rotors,plugs) 
+            i += 1
+        else : 
+            print(rotors, plugs)
+            plugs = ''
+# TODO: add the plug settings
+            i = 0
+
+print("plugs: ",plugs)
+print(rotors)
--- enigma_bare.py
+++ enigma_bare.py
@@ -1,7 +1,7 @@
 from settings import *
 
 class Enigma:
-    def encrypt(r3,r2,r1,plugs,txt):
+    def encrypt(r3,r2,r1,plugs,letter):
         shift_1 = True
         shift_2 = False
         shift_3 = False      
@@ -14,47 +14,46 @@ class Enigma:
         PLUGS = [] if PLUGS == '' else PLUGS.split()
 
         enc_txt = ""
-        for x in txt:
-            for pair in PLUGS:
-                if pair[0]== x:
-                    enc_0 = pair[1]
-                    break
-                elif pair[1] == x:
-                    enc_0 = pair[0]
-                    break
-                else:
-                    enc_0 = x
-                    break
+        for pair in PLUGS:
+            if pair[0]== letter:
+                enc_0 = pair[1]
+                break
+            elif pair[1] == letter:
+                enc_0 = pair[0]
+                break
             else:
-                enc_0 = x
+                enc_0 = letter
+                break
+        else:
+            enc_0 = letter
 
-            enc_1 = rotor_1.run_through(enc_0,True)
-            enc_2 = rotor_2.run_through(enc_1,True)
-            enc_3 = rotor_3.run_through(enc_2,True)
-            enc_4 = REFLECTOR[ord(enc_3)-ord('A')]
-            enc_5 = rotor_3.run_through(enc_4,False)
-            enc_6 = rotor_2.run_through(enc_5,False)
-            enc_7 = rotor_1.run_through(enc_6,False)
-            for pair in PLUGS:
-                if pair[0] == enc_7:
-                    enc_8 = pair[1]
-                    break
-                elif pair[1] == enc_7:
-                    enc_8 = pair[0]
-                    break
-                else:
-                    enc_8 = enc_7
-                    break
+        enc_1 = rotor_1.run_through(enc_0,True)
+        enc_2 = rotor_2.run_through(enc_1,True)
+        enc_3 = rotor_3.run_through(enc_2,True)
+        enc_4 = REFLECTOR[ord(enc_3)-ord('A')]
+        enc_5 = rotor_3.run_through(enc_4,False)
+        enc_6 = rotor_2.run_through(enc_5,False)
+        enc_7 = rotor_1.run_through(enc_6,False)
+        for pair in PLUGS:
+            if pair[0] == enc_7:
+                enc_8 = pair[1]
+                break
+            elif pair[1] == enc_7:
+                enc_8 = pair[0]
+                break
             else:
                 enc_8 = enc_7
-            
-            enc_txt += enc_8
+                break
+        else:
+            enc_8 = enc_7
+        
+        enc_txt += enc_8
 
-            shift_2 = True if rotor_1.value == 25 else False
-            shift_3 = True if rotor_2.value == 25 and rotor_1.value == 25 else False
-            rotor_1.update(shift_1)
-            rotor_2.update(shift_2)
-            rotor_3.update(shift_3)
+        shift_2 = True if rotor_1.value == 25 else False
+        shift_3 = True if rotor_2.value == rotor_1.value == 25 else False
+        rotor_1.update(shift_1)
+        rotor_2.update(shift_2)
+        rotor_3.update(shift_3)
 
         return enc_txt
         
